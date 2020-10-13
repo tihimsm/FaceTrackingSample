bfd3eccec7ccb1da0c01f7212ec308b1c091c43f
tihimsm <tihimsm@gmail.com>
2020-10-13 14:00:27 +0900

    Initial Commit

---
+++ FaceTrackingSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceTrackingSample
+//
+//  Created by Taihei Mishima on 2020/10/13.
+//  Copyright © 2020 Taihei Mishima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
